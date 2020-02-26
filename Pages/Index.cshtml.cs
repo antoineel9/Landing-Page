a34083e822748dcec17145a6d99883f4f86456d6
Antoine Williams <54339074+antoineel9@users.noreply.github.com>
2020-02-25 19:47:54 -0600

    Add files via upload

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace NWI2.Pages
+
+{
+    public class IndexModel : PageModel
+
+    {
+        public string CurrrentUserRole { get; set; }
+        public object CurrentUserRole { get; private set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            CurrentUserRole = "";
+        }
+    }
+}
